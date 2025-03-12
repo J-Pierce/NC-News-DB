d6ced069ad3c8654d3d95060736684bc699fe11e
J-Pierce <james.pierce@hotmail.co.uk>
2025-03-12 09:46:23 +0000

    now hosting apllication and provided README file

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,4 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
@@ -14,4 +13,5 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
 module.exports = new Pool(config);
