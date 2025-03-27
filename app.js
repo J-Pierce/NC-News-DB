36a8eb31aa202f09d1c4ffd3676798432f499fb3
J-Pierce <james.pierce@hotmail.co.uk>
2025-03-27 17:18:18 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const {
   handleCustomErrors,
   handleServerErrors,
 } = require("./controllers/index.controllers");
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
